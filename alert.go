333ccca41eed0d87ac625d06d0c61370716d15ce
Slava Vishnyakov <bomboze@gmail.com>
2016-12-02 09:59:34 +0300

    initial commit

---
+++ alert.go
@@ -0,0 +1,29 @@
+package alert
+
+type alert struct {
+	cap int
+	i   int
+	buf []bool
+}
+
+func NewAlert(cap int) *alert {
+	a := alert{}
+	a.i = 0
+	a.cap = cap
+	a.buf = make([]bool, cap)
+	return &a
+}
+
+func (a *alert) Append(b bool) {
+	a.i = (a.i + 1) % a.cap
+	a.buf[a.i] = b
+}
+
+func (a *alert) AllTrue() bool {
+	for _, v := range a.buf {
+		if v == false {
+			return false
+		}
+	}
+	return true
+}
---
+++ alert_test.go
@@ -0,0 +1,31 @@
+package alert
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFirst(t *testing.T) {
+	a := NewAlert(3)
+
+	assert.False(t, a.AllTrue())
+	a.Append(false)
+
+	assert.False(t, a.AllTrue())
+	a.Append(false)
+
+	assert.False(t, a.AllTrue())
+	a.Append(true)
+
+	assert.False(t, a.AllTrue())
+	a.Append(true)
+
+	assert.False(t, a.AllTrue())
+	a.Append(true)
+
+	assert.True(t, a.AllTrue())
+	a.Append(false)
+
+	assert.False(t, a.AllTrue())
+}
